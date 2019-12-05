9371756763d34eac065a18c2eac5890921b80b56
Aaron Fagan <aaron.fagan@telus.com>
2019-12-05 09:57:35 -0700

    Update entrypoint.sh

--- cron/config/entrypoint.sh
+++ cron/config/entrypoint.sh
@@ -14,8 +14,8 @@ VARS_REQUIRED=(
 
 for VAR in "${VARS_REQUIRED[@]}"; do
     if [ -z "${!VAR}" ]; then
-    	echo "ERROR: Required variable \"${VAR}\" is not set!";
-    	VAR_ERROR=true;
+    	echo "ERROR: Required variable \"${VAR}\" is not set!"
+    	VAR_ERROR=true
     fi
 done
 if [ "${VAR_ERROR}" ]; then exit 1; fi
@@ -38,14 +38,14 @@ CRON_ACCEPT=(
 for CRON in $(echo ${CRONS} | sed -e "s/,/ /g" -e "s/  / /g"); do
 	CRON=$(echo ${CRON#*_} | tr A-Z a-z)
     if [[ ! " ${CRON_ACCEPT[@]} " =~ " ${CRON} " ]]; then
-    	echo "Cron \"${CRON}\" is not acceptable. Skipping...";
+    	echo "Cron \"${CRON}\" is not acceptable. Skipping..."
     else
 		DIR="/root/scripts/${CRON}"
 		mkdir -p ${DIR}
 		if [ "${CRON}" == "minutely" ]; then
-			TIME="* * * * *";
+			TIME="* * * * *"
 		else
-			TIME="@${CRON}";
+			TIME="@${CRON}"
 		fi
 		echo "${TIME} root /usr/bin/find ${DIR} -path ${DIR}/_* -prune -o -type f ! -iname "_*.sh" -iname "*.sh" -exec chmod +x {} \; -exec bash {} \; > /proc/1/fd/1" > /etc/cron.d/cron-${CRON}
     fi
@@ -53,5 +53,5 @@ done
 
 service cron start > /dev/null 2>&1
 
-echo "[$(date +'%F %T')] Cron is running!"
+echo "Cron is running!"
 exec $(which tail) -f /dev/null
